7d35ab26d3b0a0719da5bd6200e08062cd3d6d6f
Gamunu Bandara <gamunu.19@cse.mrt.ac.lk>
2023-07-18 22:34:21 +0530

    add auth logic

--- src/main/java/com/mosipcse/fingerprintutils/FingerPrintHandler.java
+++ src/main/java/com/mosipcse/fingerprintutils/FingerPrintHandler.java
@@ -1,5 +1,59 @@
 package com.mosipcse.fingerprintutils;
 
+
+import com.machinezoo.sourceafis.FingerprintImage;
+import com.machinezoo.sourceafis.FingerprintMatcher;
+import com.machinezoo.sourceafis.FingerprintTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class FingerPrintHandler {
+    private ArrayList<IdentityRecord> fingerprintDb;
+    public String findMatchingPrint(String filepath) {
+        System.out.println("Matching in progress");
+        FingerprintMatcher matcher = getFingerPrintMatcher(filepath);
+        IdentityRecord successfulMatch = matchFingerprint(matcher) ;
+        if (successfulMatch == null) {
+            return null ;
+        }
+        return successfulMatch.getId();
+    }
+
+    private IdentityRecord matchFingerprint(FingerprintMatcher matcher) {
+        IdentityRecord matchingId = null;
+        double max = Double.NEGATIVE_INFINITY;
+        for (IdentityRecord candidate : this.fingerprintDb) {
+            ArrayList<FingerprintTemplate> fingerprints = candidate.getFingerprints();
+            for (FingerprintTemplate template : fingerprints) {
+                double similarity = matcher.match(template);
+                if (similarity > max) {
+                    max = similarity;
+                    matchingId = candidate;
+                }
+            }
+        }
+        double threshold = 40;
+        return max >= threshold ? matchingId : null;
+    }
+
+    public String enterNewRecord(String filepath) {
+        return "id" ;
+    }
+
+    private FingerprintMatcher getFingerPrintMatcher(String filepath) {
+        FingerprintImage image;
+        try {
+            image = new FingerprintImage(Files.readAllBytes(Paths.get(filepath)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        FingerprintTemplate template = new FingerprintTemplate(image);
+        return new FingerprintMatcher(template);
+    }
+
+
 
 }
