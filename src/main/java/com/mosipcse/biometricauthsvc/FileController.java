fe3a8627e7818eb333fc7cb1eb4835048cc70a3c
AwesomeNipun <nipun1969@gmail.com>
2023-08-10 12:14:55 +0530

    Add save fingerprint functionality

--- src/main/java/com/mosipcse/biometricauthsvc/FileController.java
+++ src/main/java/com/mosipcse/biometricauthsvc/FileController.java
@@ -1,18 +1,19 @@
 package com.mosipcse.biometricauthsvc;
 
-import com.mosipcse.fileHandler.FileUploadResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class FileController {
@@ -22,44 +23,114 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
 
-        if (file.isEmpty()) {
-            return ResponseEntity.badRequest().body("File is empty");
-        }
+    public ResponseEntity<Object> uploadDataWithIndex(@RequestBody UploadRequest uploadRequest) {
 
         try {
-            // Get the file name
-            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            String projectDirectory = System.getProperty("user.dir"); // Get the project directory
+            String targetDirectory = "fingerprints"; // Name of the target directory
 
-            // Define the file path
-            String baseDirectory = System.getProperty("user.home");
-            String targetDirectory = "Desktop/fingerPrints";
-
-            // creating target directory if it does not exist
-            Path fullPath = Paths.get(baseDirectory, targetDirectory);
+            // Creating target directory if it does not exist
+            Path fullPath = Paths.get(projectDirectory, targetDirectory);
 
             try {
                 if (!Files.exists(fullPath)) {
                     Files.createDirectories(fullPath);
                 }
             } catch (IOException e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save file: Unable to " +
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save data: Unable to " +
                         "create target directory");
             }
 
-            // Define the file path within the target directory
-            Path filePath = fullPath.resolve(fileName);
+            String index = uploadRequest.getIndex();
 
-            // Save the file to the specified path
-            Files.copy(file.getInputStream(), filePath);
-            String responseMessage = "File saved successfully.";
-            FileUploadResponse response = new FileUploadResponse(responseMessage, filePath.toString());
+            if (index == null || index.isEmpty()) {
+                String random;
+                Path randomPath;
+                do {
+                    random = String.valueOf(new Random().nextInt(100000));
+                    randomPath = fullPath.resolve(random);
+                } while (Files.exists(randomPath));
+
+                for (Data entry : uploadRequest.getData()) {
+
+                    String imageFileName = entry.getBioSubType().replace(" ", "_") + ".jpg";
+
+                    Path subFolderPath = fullPath.resolve(random);
+                    Files.createDirectories(subFolderPath);
+
+                    Path imagePath = subFolderPath.resolve(imageFileName);
+                    Files.write(imagePath, entry.getBuffer().getData());
+                }
+            } else {
+                for (Data entry : uploadRequest.getData()) {
+                    // Create a subfolder based on the provided index
+                    Path subFolderPath = fullPath.resolve(index);
+                    Files.createDirectories(subFolderPath);
+
+                    // Create a unique filename for the image
+                    String imageFileName = entry.getBioSubType().replace(" ", "_") + ".jpg";
+                    Path imagePath = subFolderPath.resolve(imageFileName);
+
+                    // Save the image buffer as a JPEG file
+                    Files.write(imagePath, entry.getBuffer().getData());
+
+                    // You can now do something with the imagePath if needed
+                }
+            }
+
+            String responseMessage = "Files saved successfully.";
+            Map<String, String> response = new HashMap<>();
+            response.put("message", responseMessage);
 
             return ResponseEntity.ok(response);
 
         } catch (IOException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save file");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save files");
         }
     }
+}
+
+class UploadRequest {
+    private String index;
+    private List<Data> data;
+
+    public String getIndex() {
+        return index;
+    }
+
+    public List<Data> getData() {
+        return data;
+    }
+
+    // Constructors, getters, and setters for index and data
+}
+
+class Data {
+    private Buffer buffer;
+    private String bioSubType;
+
+    public String getBioSubType() {
+
+        return bioSubType;
+    }
+
+    public Buffer getBuffer() {
+
+        return buffer;
+    }
+
+    // Constructors, getters, and setters for buffer and bioSubType
+}
+
+class Buffer {
+    private String type;
+    private byte[] data;
+
+    public byte[] getData() {
+
+        return data;
+    }
+
+    // Constructors, getters, and setters for type and data
 }
